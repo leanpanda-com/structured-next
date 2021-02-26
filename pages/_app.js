834a6605f07b979209f02531a2deed50ae1c8c11
Joe Yates <joe.g.yates@gmail.com>
2021-02-26 11:11:05 +0100

    Apply layout to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import Footer from '../components/Footer'
+import styles from '../styles/Home.module.css'
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>DatoCMS Structured Text</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <Footer />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,19 @@
-import Head from 'next/head'
 import Link from 'next/link'
 
-import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>DatoCMS Structured Text</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        DatoCMS Structured Text
+      </h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          DatoCMS Structured Text
-        </h1>
-
-        <p className={styles.examples}>
-          <Link href="/articles">
-            Examples
-          </Link>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+      <p className={styles.examples}>
+        <Link href="/articles">
+          Examples
+        </Link>
+      </p>
+    </main>
   )
 }
