65e7c4d40774bc6fd4d6ecc8c1bf0226c57b4916
Joe Yates <joe.g.yates@gmail.com>
2021-02-26 11:12:15 +0100

    Correct URL

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 const Footer = () => (
   <footer className={styles.footer}>
     <a
-      href="https://leanpanda.com"
+      href="https://www.leanpanda.com"
       target="_blank"
       rel="noopener noreferrer"
     >
